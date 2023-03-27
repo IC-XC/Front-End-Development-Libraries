44c1794c5f9cbbec235cc89ded84ae2258f91d83
Per <86513555+IC-XC@users.noreply.github.com>
2023-03-27 18:19:17 +0200

    Add files via upload

---
+++ 02 jQuery/script.js
@@ -0,0 +1,22 @@
+
+ $(document).ready(function() {
+    /*$("button").addClass("animated");
+    $(".btn").addClass("shake");
+    $("#target1").addClass("btn-primary");*/
+
+    /*$("button").addClass("animated bounce");
+    $(".well").addClass("animated shake");
+    $("#target3").addClass("animated fadeOut");
+    $("button").removeClass("btn-default");*/
+
+    $("#target1").css("color", "red");
+    $("#target1").prop("disabled", true);
+    $("#target4").remove();
+    $("#target2").appendTo("#right-well");
+    $("#target5").clone().appendTo("#left-well");
+    $("#target1").parent().css("background-color", "red");
+    $("#right-well").children().css("color", "orange");
+    $(".target:nth-child(2)").addClass("animated bounce");
+    $(".target:even").addClass("animated shake");
+    $("body").addClass("animated hinge");
+ });
